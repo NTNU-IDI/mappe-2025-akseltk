c0e12b064676433617988bfda86a3af6a243cce9
Aksel Kirkhorn <akseltk@stud.ntnu.no>
2025-12-04 14:03:38 +0100

    refactor: update App to inject registers and ui into DiaryController, replace ui init and start

--- src/main/java/edu/ntnu/idi/idatt/App.java
+++ src/main/java/edu/ntnu/idi/idatt/App.java
@@ -1,11 +1,17 @@
 package edu.ntnu.idi.idatt;
 
+import edu.ntnu.idi.idatt.controller.DiaryController;
+import edu.ntnu.idi.idatt.model.AuthorRegister;
+import edu.ntnu.idi.idatt.model.DiaryRegister;
 import edu.ntnu.idi.idatt.view.UserInterface;
 
 public class App {
-  public static void main(String[] args) {
-    UserInterface ui = new UserInterface();
-    ui.init();
-  }
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        DiaryRegister diaryRegister = new DiaryRegister();
+        AuthorRegister authorRegister = new AuthorRegister();
+        
+        DiaryController controller = new DiaryController(diaryRegister, authorRegister, ui);
+        controller.start();
+    }
 }
-
