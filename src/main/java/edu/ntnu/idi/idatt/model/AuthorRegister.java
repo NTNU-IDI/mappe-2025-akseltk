3acb524abac524cf2f216828f6bf3883bfa909a0
Aksel Kirkhorn <akseltk@stud.ntnu.no>
2025-12-04 11:25:34 +0100

    refactor: fix indentation error and add email duplicate check

--- src/main/java/edu/ntnu/idi/idatt/model/AuthorRegister.java
+++ src/main/java/edu/ntnu/idi/idatt/model/AuthorRegister.java
@@ -2,31 +2,38 @@ package edu.ntnu.idi.idatt.model;
 
 import edu.ntnu.idi.idatt.util.Validators;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class AuthorRegister {
-    private final List<Author> authors;
+  private final List<Author> authors;
 
-    public AuthorRegister() {
-        this.authors = new ArrayList<>();
+  public AuthorRegister() {
+    this.authors = new ArrayList<>();
+  }
+
+  public void addAuthor(Author author) {
+    Validators.validateNotNull(author, "Author");
+
+    boolean emailExists = authors.stream()
+            .anyMatch(a -> a.getEmail().equalsIgnoreCase(author.getEmail()));
+
+    if (emailExists) {
+      throw new IllegalArgumentException("Author with email " + author.getEmail() + " already exists.");
     }
 
-    public void addAuthor(Author author) {
-        Validators.validateNotNull(author, "Author");
-        authors.add(author);
-    }
+    authors.add(author);
+  }
 
-    public void removeAuthor(Author author) {
-        Validators.validateNotNull(author, "Author");
-        authors.remove(author);
-    }
+  public void removeAuthor(Author author) {
+    Validators.validateNotNull(author, "Author");
+    authors.remove(author);
+  }
 
-    public List<Author> getAllAuthors() {
-        return authors.stream()
-                .sorted(Comparator.comparing(Author::getFullName))
-                .toList();
-    }
+  public List<Author> getAllAuthors() {
+    return authors.stream()
+            .sorted(Comparator.comparing(Author::getFullName))
+            .toList();
+  }
 }
