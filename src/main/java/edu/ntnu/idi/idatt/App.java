7cb9d6a08d178ed9c3c178d91b92a2d4c1e4e1db
Aksel Kirkhorn <akseltk@stud.ntnu.no>
2025-12-04 17:36:20 +0100

    refracture: fix indentation level

--- src/main/java/edu/ntnu/idi/idatt/App.java
+++ src/main/java/edu/ntnu/idi/idatt/App.java
@@ -6,12 +6,12 @@ import edu.ntnu.idi.idatt.model.DiaryRegister;
 import edu.ntnu.idi.idatt.view.UserInterface;
 
 public class App {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        DiaryRegister diaryRegister = new DiaryRegister();
-        AuthorRegister authorRegister = new AuthorRegister();
-        
-        DiaryController controller = new DiaryController(diaryRegister, authorRegister, ui);
-        controller.start();
-    }
+  public static void main(String[] args) {
+    UserInterface ui = new UserInterface();
+    DiaryRegister diaryRegister = new DiaryRegister();
+    AuthorRegister authorRegister = new AuthorRegister();
+
+    DiaryController controller = new DiaryController(diaryRegister, authorRegister, ui);
+    controller.start();
+  }
 }
